f70e0c14bd735f74b61c91ae7246e3055a9f6fd9
O Dodart <olivia.dodart@gmail.com>
2023-10-17 15:00:08 -0400

    end of playlist

--- Iteration3/basic.js
+++ Iteration3/basic.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     /* progress bar changes over time */
     let prog = document.getElementsByClassName("progress")[0];
+    let list = document.getElementById("playlist").children[0];
     let w = 0;
     let s = 1;
     let dur = Math.ceil(Math.random() * 50) + 50;
@@ -8,17 +9,17 @@ document.addEventListener("DOMContentLoaded", () => {
     setInterval(() => {
         w += (dur/100);
         prog.style.width = w + "%";
-        if(w > 100) { /*progress bar restarts and moves next song up*/
+        if(w > 100 && list.children.length > 1) { /*progress bar restarts and moves next song up*/
             w = 0;
             s++;
             document.getElementById("current").children[0].children[0].innerHTML = "Song " + s;
-            let list = document.getElementById("playlist").children[0];
+            list = document.getElementById("playlist").children[0];
             list.removeChild(list.children[1]);
             dur = Math.ceil(Math.random() * 50) + 50;
         }
-        if(s == 8) {
-            document.getElementById("current").children[0].children[0].innerHTML = "Stay Tuned for More Songs!";
+        if(list.children.length == 1) {
             w=100;
+            document.getElementById("current").children[0].children[0].innerHTML = "Stay Tuned for More Songs!";
         }
-    }, dur)
+    }, 1)
 });
