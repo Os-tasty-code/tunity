32f78104cb5ec34aa70ea7a59a500223f2b46d6e
O Dodart <olivia.dodart@gmail.com>
2023-10-17 14:35:47 -0400

    randomized times

--- Iteration3/basic.js
+++ Iteration3/basic.js
@@ -3,19 +3,22 @@ document.addEventListener("DOMContentLoaded", () => {
     let prog = document.getElementsByClassName("progress")[0];
     let w = 0;
     let s = 1;
+    let dur = Math.ceil(Math.random() * 50) + 50;
+    console.log(dur);
     setInterval(() => {
-        w += 0.5;
+        w += (dur/100);
         prog.style.width = w + "%";
-        if(w == 100) { /*progress bar restarts and moves next song up*/
+        if(w > 100) { /*progress bar restarts and moves next song up*/
             w = 0;
             s++;
             document.getElementById("current").children[0].children[0].innerHTML = "Song " + s;
             let list = document.getElementById("playlist").children[0];
             list.removeChild(list.children[1]);
+            dur = Math.ceil(Math.random() * 50) + 50;
         }
         if(s == 8) {
             document.getElementById("current").children[0].children[0].innerHTML = "Stay Tuned for More Songs!";
             w=100;
         }
-    }, 50)
+    }, dur)
 });
