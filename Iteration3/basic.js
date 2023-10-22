14ec90ec496e884b3acbb9254bf0c1fe56615c7d
O Dodart <olivia.dodart@gmail.com>
2023-10-22 12:52:17 -0400

    song changes complete for iteration 3

--- Iteration3/basic.js
+++ Iteration3/basic.js
@@ -1,38 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
     /* start song playing */
+    let song = document.getElementById("song-audio")
+    song.src = "Song-1.mp3";
     toggle();
+    // loadMusic();
     /* progress bar changes over time */
     let prog = document.getElementsByClassName("progress")[0];
     let list = document.getElementById("playlist").children[0];
     let w = 0;
     let s = 1;
-    let dur = document.getElementById("song-audio").duration;
-    console.log(dur);
+    let dur = 100;
+    // if(song.duration != NaN) {
+    //     dur = song.duration
+    // }
     setInterval(() => {
         w += (dur/100);
-        prog.style.width = w + "%";
+        prog.value = w;
         if(w > 100 && list.children.length > 1) { /*progress bar restarts and moves next song up*/
             w = 0;
             s++;
-            document.getElementById("current").children[0].children[0].innerHTML = "Song " + s;
+            document.getElementById("current").children[0].children[1].innerHTML = "Song " + s;
             list = document.getElementById("playlist").children[0];
             list.removeChild(list.children[1]);
-            dur = Math.ceil(Math.random() * 50) + 50;
+            dur = song.duration;
         }
         if(list.children.length == 1) {
             w=100;
             document.getElementById("current").children[0].children[0].innerHTML = "Stay Tuned for More Songs!";
         }
-    }, 1)
+    }, 100)
 });
+
+function loadMusic() {
+    let song = document.getElementById("song-audio")
+    song.src = "Song-1.mp3";
+    toggle();
+    // console.log(song.duration);
+}
 
 /*onclick function for play button*/
 
 function toggle() {
     let button = document.getElementById("toggle-song");
     let song = document.getElementById("song-audio");
-    console.log(song);
-    button.innerHTML = "▶";
-    song.setAttribute("src", "Song-1.mp3");
-    song.play();
+    if(song.paused) {
+        button.innerHTML = "||";
+        song.play();
+    } else {
+        button.innerHTML = "▶";
+        song.pause();
+    }
 }
